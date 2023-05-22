731e5114546b4617c0e4f742f7ff30c74e6dcbf5
Nicollas Tavares da Silva <nicollascursos@gmail.com>
2023-05-22 19:07:16 -0300

    Criei um teclado virtual

---
+++ teclado.js
@@ -0,0 +1,9 @@
+function aperta(lt) {
+    document.getElementById("aperta").innerHTML = `Você apertou a letra ${lt}`;
+    document.getElementById("btn").innerHTML = `SAIR`;
+}
+
+function solta(lt){
+    document.getElementById("solta").innerHTML = "Você soltou a tecla " +lt;
+}
+
